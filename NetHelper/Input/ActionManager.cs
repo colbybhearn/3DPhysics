aa4820d85e1c1993adc5dda6bbc3920d90b74d73
Jeffrey <goku75703@hotmail.com>
2012-09-14 23:15:26 -0500

    Fixed one of Colby's bugs

--- NetHelper/Input/ActionManager.cs
+++ NetHelper/Input/ActionManager.cs
@@ -135,6 +135,7 @@ namespace Helper.Input
         /// <param name="actionVals"></param>
         public void ProcessActionValues(object[] actionVals)
         {
+            ActionValues = new List<object>(actionVals);
             foreach (ActionBinding ab in Bindings.Values)
                 ab.Callback(GetAliasDelegateValues(ab.ID));
         }
